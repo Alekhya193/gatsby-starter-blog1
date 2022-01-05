b3a3fdb3a1bebe2dced0e0e5ca7a89f0dce24726
Alekhya <alekhyasiddi193@gmail.com>
2022-01-05 14:42:46 +0530

    changes done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Gatsby Starter Blog`, //title of the blog
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kyle Mathews`,  //name of the author
+      summary: `who lives and works in San Francisco building useful things.`,//summary of the blog
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kylemathews`, //twitter acount
     },
   },
   plugins: [
